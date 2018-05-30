9ba92e9796f5f18391311db40477f2487ca75d20
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-05-29 23:43:17 -0600

    deleted user/auth section

--- server/models/user.js
+++ server/models/user.js
@@ -1,81 +1,81 @@
-let mongoose = require('mongoose')
-let Schema = mongoose.Schema
-let ObjectId = Schema.Types.ObjectId
-let bcrypt = require('bcryptjs')
-var schemaName = 'User'
-const SALT_FACTOR = 10
+// let mongoose = require('mongoose')
+// let Schema = mongoose.Schema
+// let ObjectId = Schema.Types.ObjectId
+// let bcrypt = require('bcryptjs')
+// var schemaName = 'User'
+// const SALT_FACTOR = 10
 
-// const USERROLES = [
-//   "Guest",
-//   "User",
-// ]
+// // const USERROLES = [
+// //   "Guest",
+// //   "User",
+// // ]
 
-// const ADMINROLES = [
-//   "Admin"
-// ]
+// // const ADMINROLES = [
+// //   "Admin"
+// // ]
 
-// const ROLES = [...USERROLES, ...ADMINROLES]
+// // const ROLES = [...USERROLES, ...ADMINROLES]
 
-let schema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, dropDups: true },
-  password: { type: String, required: true },
-  created: { type: Number, required: true, default: Date.now() },
-  role: {
-    type: String, required: true, enum:
-      [
-        'Guest',
-        'User',
-        'Admin',
-      ],
-    default: 'Guest',
-  }
-})
-
-// schema.pre('save', function (next) {
-//   var user = this;
-//   if (!user.isModified('password')) {
-//     return next();
+// let schema = new Schema({
+//   name: { type: String, required: true },
+//   email: { type: String, required: true, unique: true, dropDups: true },
+//   password: { type: String, required: true },
+//   created: { type: Number, required: true, default: Date.now() },
+//   role: {
+//     type: String, required: true, enum:
+//       [
+//         'Guest',
+//         'User',
+//         'Admin',
+//       ],
+//     default: 'Guest',
 //   }
-//   bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
-//     if (err) {
-//       return next(err);
-//     } else {
-//       bcrypt.hash(user.password, salt, function (err, hash) {
-//         user.password = hash;
-//         next();
-//       });
-//     }
-//   });
-// });
+// })
 
-schema.methods.validatePassword = function (password) {
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, this.password, function (err, isMatch) {
-      if (err || !isMatch) {
-        return reject(err);
-      }
-      return resolve(isMatch);
-    });
-  })
-};
+// // schema.pre('save', function (next) {
+// //   var user = this;
+// //   if (!user.isModified('password')) {
+// //     return next();
+// //   }
+// //   bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+// //     if (err) {
+// //       return next(err);
+// //     } else {
+// //       bcrypt.hash(user.password, salt, function (err, hash) {
+// //         user.password = hash;
+// //         next();
+// //       });
+// //     }
+// //   });
+// // });
 
-// schema.methods.changeRank = function (reqRole) {
-//   var currentRole = this.role
-//   if (ROLES.indexOf(reqRole) > ROLES.indexOf(currentRole)) {
-//     return currentRole
-//   }
-//   return reqRole
-// }
+// schema.methods.validatePassword = function (password) {
+//   return new Promise((resolve, reject) => {
+//     bcrypt.compare(password, this.password, function (err, isMatch) {
+//       if (err || !isMatch) {
+//         return reject(err);
+//       }
+//       return resolve(isMatch);
+//     });
+//   })
+// };
 
-// schema.methods.setRoleForOther = function (other, userType) {
+// // schema.methods.changeRank = function (reqRole) {
+// //   var currentRole = this.role
+// //   if (ROLES.indexOf(reqRole) > ROLES.indexOf(currentRole)) {
+// //     return currentRole
+// //   }
+// //   return reqRole
+// // }
 
-//   var isAdmin = ADMINROLES.includes(this.role)
-//   var canChangeRole = isAdmin && ROLES.indexOf(this.role) > ROLES.indexOf(other.role)
-//   if (canChangeRole) {
-//     other.role = this.changeRole(userType)
-//     return true
-//   }
-// }
+// // schema.methods.setRoleForOther = function (other, userType) {
 
-module.exports = mongoose.model('User', schema)
+// //   var isAdmin = ADMINROLES.includes(this.role)
+// //   var canChangeRole = isAdmin && ROLES.indexOf(this.role) > ROLES.indexOf(other.role)
+// //   if (canChangeRole) {
+// //     other.role = this.changeRole(userType)
+// //     return true
+// //   }
+// // }
+
+// module.exports = mongoose.model('User', schema)
--- server/routes/playlists.js
+++ server/routes/playlists.js
@@ -1,6 +1,6 @@
 var router = require('express').Router()
 var Playlists = require('../models/playlist')
-var Users = require('../models/user')
+// var Users = require('../models/user')
 let session = require('../authentication/sessions')
 
 //get & get by id
@@ -29,26 +29,26 @@ router.get('/api/playlists/:id?', (req, res) => {
 })
 
 //EDIT
-router.post('/api/create-playlist', (req, res) => {
-  Users.findById(req.session.uid)
-    .then(user => {
-      if (!user) {
-        return res.status(400).send({ message: "invalid action" })
-      } else if (user.role != 'Admin') {
-        return res.status(400).send({ message: "invalid action" })
-      }
-      Playlists.create(req.body)
-        .then(newPlaylist => {
-          res.status(200).send(newPlaylist)
-        })
-        .catch(err => {
-          res.status(400).send(err)
-        })
-    })
-    .catch(err => {
-      res.status(400).send(err)
-    })
-}),
+// router.post('/api/create-playlist', (req, res) => {
+//   Users.findById(req.session.uid)
+//     .then(user => {
+//       if (!user) {
+//         return res.status(400).send({ message: "invalid action" })
+//       } else if (user.role != 'Admin') {
+//         return res.status(400).send({ message: "invalid action" })
+//       }
+//       Playlists.create(req.body)
+//         .then(newPlaylist => {
+//           res.status(200).send(newPlaylist)
+//         })
+//         .catch(err => {
+//           res.status(400).send(err)
+//         })
+//     })
+//     .catch(err => {
+//       res.status(400).send(err)
+//     })
+// }),
 
   // router.post('/playlists', (req, res, next) => {
   //   Playlists.create(req.body).then(playlist => {
